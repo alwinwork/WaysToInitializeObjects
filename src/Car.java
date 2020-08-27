852855a38078253c4b8cc5f58880ea2bc2bd17b0
alwinwork <alwin.solanky@sogeti.com>
2020-08-27 12:53:04 +0100

    Constructor

--- src/Car.java
+++ src/Car.java
@@ -4,17 +4,17 @@ public class Car {
     public String colour;
     public int seats;
 
+    Car(String model, String colour, int seats){
+        this.model = model;
+        this.colour = colour;
+        this.seats = seats;
+    }
+
     public void display(){
         System.out.println("Car Model is : " + model);
         System.out.println("The car colour is : " + colour);
         System.out.println("Number of seats are : " + seats);
         System.out.println();
-    }
-
-    public void initialize(String a3_quattro, String green, int i){
-        model = a3_quattro;
-        colour = green;
-        seats = i;
     }
 
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,17 +1,9 @@
 public class MainClass {
     public static void main(String[] args){
 
-        Car c = new Car();
-        //c.initialize("Model s", "Red", 4);
-        c.model = "Model s" ;
-        c.colour = "Red";
-        c.seats = 4;
-
-        Car c1 = new Car();
-        c1.initialize("Model S2", "Yellow", 3);
-
+      //constructor
+        Car c = new Car("Tesla S", "Brown", 6);
         c.display();
-        c1.display();
 
     }
 }
