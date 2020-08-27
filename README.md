a671805ff2e86488da7c7e9072a48897ba38f7d0
alwinwork <alwin.solanky@sogeti.com>
2020-08-27 11:46:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ways To Initialize Objects
---
+++ src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+    public String model;
+    public String colour;
+    public int seats;
+
+    public void display(){
+        System.out.println("Car Model is : " + model);
+        System.out.println("The car colour is : " + colour);
+        System.out.println("Number of seats are : " + seats);
+        System.out.println();
+    }
+
+}
