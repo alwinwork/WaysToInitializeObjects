81112f42e8a77f96e37b52b8c7fe7a8a9e3a1471
alwinwork <alwin.solanky@sogeti.com>
2020-08-27 12:24:06 +0100

    More initialisations

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,7 +2,10 @@ public class MainClass {
     public static void main(String[] args){
 
         Car c = new Car();
-        c.initialize("Model s", "Red", 4);
+        //c.initialize("Model s", "Red", 4);
+        c.model = "Model s" ;
+        c.colour = "Red";
+        c.seats = 4;
 
         Car c1 = new Car();
         c1.initialize("Model S2", "Yellow", 3);
